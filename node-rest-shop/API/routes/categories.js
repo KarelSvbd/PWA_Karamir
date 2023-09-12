2a645c8c56c1b3988d5373db4c3afdcaf424d078
karelsvbd <karel.svbd@eduge.ch>
2023-09-12 08:58:41 +0200

    hotfix: added get route for categories

--- node-rest-shop/API/routes/categories.js
+++ node-rest-shop/API/routes/categories.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
-
+//get all categories
+router.get('/', (req, res, next) => {
+    res.status(200).json({
+        message: 'Not implemented route'
+    });
+});
 
 //Adding a category
 router.post('/', (req, res, next) => {
