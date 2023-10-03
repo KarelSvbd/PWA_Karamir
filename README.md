bfa6db24741f9aded67aa47ae7dbe3be5b41e0d6
karelsvbd <karel.svbd@eduge.ch>
2023-10-03 10:23:25 +0200

    code: working on the app

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # PWA_Karamir
+
+Autheurs : Amir Younes / Karel Vilém Svoboda
+Classe : T.IS-E2A
+Cours : PDA
--- frontend/index.js
+++ frontend/index.js
@@ -1,29 +1,27 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.hello = void 0;
-const world = 'world';
-function hello(who = world) {
-    return `Hello ${who}! `;
-}
-exports.hello = hello;
-// open a connection to the database
-const request = indexedDB.open("myDatabase", 1);
-// handle errors that may occur when opening the database
-request.onerror = (event) => {
-    console.error("Database error:", event.target.errorCode);
-};
-// get the data from the object store
-request.onsuccess = (event) => {
-    const db = event.target.result;
-    const transaction = db.transaction("myObjectStore", "readonly");
-    const objectStore = transaction.objectStore("myObjectStore");
-    const request = objectStore.getAll();
-    request.onsuccess = (event) => {
-        const data = event.target.result;
-        console.log(data);
-    };
-};
-fetch('http://10.5.51.40:3000/notes')
-    .then(response => response.json())
-    .then(data => console.log(data))
-    .catch(error => console.error(error));
+const apiUrl = 'http://10.5.51.40:3000/courses';
+fetch(apiUrl)
+    .then(response => {
+    if (!response.ok) {
+        throw new Error('HTTP error ' + response.status);
+    }
+    return response.json();
+})
+    .then(data => {
+    data.forEach((cours) => {
+        const listecours = document.querySelector("#listecours");
+        listecours.innerHTML += `
+        <div class="w3-third w3-container w3-margin-bottom">
+          <img src="csharpicon.png" alt="Norway" style="width:100%" class="w3-hover-opacity">
+          <div class="w3-container w3-white">
+            <p><b>${cours['category']}, ${cours['name']}</b></p>
+            <p>${cours['description']}</p>
+          </div>
+        </div>
+      `;
+        console.log(cours);
+    });
+})
+    .catch(error => {
+    console.error('Error fetching data from API:', error);
+});
